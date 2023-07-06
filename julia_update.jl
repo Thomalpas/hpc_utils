b9c40293c12c144b1dc735c3c7995adca224aeb9
Thomalpas <92929876+Thomalpas@users.noreply.github.com>
2023-07-06 15:00:28 +0100

    Update julia_update.jl

--- julia_update.jl
+++ julia_update.jl
@@ -2,11 +2,3 @@
 # See readme 
 using Pkg
 Pkg.update()
-
-using BEFWM2
-
-fw = FoodWeb(nichemodel, 10, C = 0.2, Z = 10)
-
-st = AddStochasticity(fw, addstochasticity = true, target = "producers", θ = 1.2, σe = 0.4)
-
-println(st.addstochasticity)

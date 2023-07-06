9287137bb9c6b61f90a83e11f3b6bb8907418fe3
Thomalpas <92929876+Thomalpas@users.noreply.github.com>
2023-07-06 15:09:55 +0100

    Create julia_stoch_BEFWM2_test.jl

---
+++ julia_stoch_BEFWM2_test.jl
@@ -0,0 +1,17 @@
+# Test for BEFWM2
+
+using BEFWM2, Arrow
+
+fw = FoodWeb(nichemodel, 10, C = 0.2, Z = 10)
+
+st = AddStochasticity(fw, addstochasticity = true, target = "producers", θ = 1.2, σe = 0.4)
+
+mp = ModelParameters(fw, stochasticity = st)
+
+B0 = repeat([0.5], 10)
+
+sol = simulate(mp, B0, tmax = 50)
+
+open("output.txt", "w") do file
+    write(file, string(sol.t[end]))
+end

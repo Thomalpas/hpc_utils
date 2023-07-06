d127836901ca595d4d98b7471f5cf6d91ad50a55
Thomalpas <92929876+Thomalpas@users.noreply.github.com>
2023-07-06 14:34:30 +0100

    Create julia_update.sh
    file for updating Pkg

---
+++ sharc/julia_update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Job name
+#$ -N Pkg.add()
+
+# Error message
+#$ -e pkg_add.error
+#$ -o pkg_add.out
+
+# Amount of RAM requested per node
+#$ -l rmem=16G
+
+# Replace by the path to the folder where your script lives if necessary
+DIR_SCRIPT=/home/${USER}/hpc_utils
+
+# Load modules
+module load apps/julia/1.8.5/binary
+
+julia ${DIR_SCRIPT}/julia_update.jl
